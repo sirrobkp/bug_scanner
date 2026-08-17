dfc26cea37fb46515757038795e13fd8e57db057
Robert <robert@192.168.1.4>
2026-08-17 23:32:38 +0300

    version-3

--- lib/app.dart
+++ lib/app.dart
@@ -67,89 +67,63 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
     final statusBarHeight = MediaQuery.of(context).padding.top;
     
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width > 390
-              ? 390
-              : MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height > 844
-              ? 844
-              : MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: const Color(0xFF0D0D0D),
-            borderRadius: BorderRadius.circular(
-              MediaQuery.of(context).size.width > 390 ? 44 : 0,
+      backgroundColor: const Color(0xFF0D0D0D),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: const Color(0xFF0D0D0D),
+        child: Stack(
+          children: [
+            // Scan line overlay
+            IgnorePointer(
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.transparent,
+                      Color.fromRGBO(0, 0, 0, 0.03),
+                      Colors.transparent,
+                    ],
+                    stops: [0.0, 0.5, 1.0],
+                  ),
+                ),
+                child: CustomPaint(
+                  painter: ScanLinePainter(),
+                ),
+              ),
             ),
-            boxShadow: const [
-              BoxShadow(
-                color: Color.fromRGBO(0, 255, 102, 0.08),
-                spreadRadius: 0,
-                blurRadius: 0,
-                offset: Offset(0, 0),
-              ),
-              BoxShadow(
-                color: Colors.black,
-                spreadRadius: 0,
-                blurRadius: 60,
-              ),
-            ],
-          ),
-          child: Stack(
-            children: [
-              // Scan line overlay
-              IgnorePointer(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                      MediaQuery.of(context).size.width > 390 ? 44 : 0,
-                    ),
-                    gradient: const LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Color.fromRGBO(0, 0, 0, 0.03),
-                        Colors.transparent,
-                      ],
-                      stops: [0.0, 0.5, 1.0],
-                    ),
-                  ),
-                  child: CustomPaint(
-                    painter: ScanLinePainter(),
-                  ),
-                ),
-              ),
-              
-              // Screen area - starts after status bar space
-              Positioned(
-                top: statusBarHeight,  // Preserve space for status bar
-                left: 0,
-                right: 0,
-                bottom: 72,
-                child: ClipRect(
-                  child: AnimatedOpacity(
-                    opacity: _transitioning ? 0 : 1,
+            
+            // Screen area - starts after status bar space
+            Positioned(
+              top: statusBarHeight,
+              left: 0,
+              right: 0,
+              bottom: 72,
+              child: ClipRect(
+                child: AnimatedOpacity(
+                  opacity: _transitioning ? 0 : 1,
+                  duration: const Duration(milliseconds: 180),
+                  child: AnimatedScale(
+                    scale: _transitioning ? 0.97 : 1.0,
                     duration: const Duration(milliseconds: 180),
-                    child: AnimatedScale(
-                      scale: _transitioning ? 0.97 : 1.0,
-                      duration: const Duration(milliseconds: 180),
-                      child: _getScreen(_activeScreen),
-                    ),
+                    child: _getScreen(_activeScreen),
                   ),
                 ),
               ),
-              
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: BottomNav(
-                  active: _activeScreen,
-                  onNavigate: _navigate,
-                ),
+            ),
+            
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: BottomNav(
+                active: _activeScreen,
+                onNavigate: _navigate,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
